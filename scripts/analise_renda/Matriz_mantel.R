5f6fb7f49ccc0c4d42fbf3adc85d06d61fdbc0b2
NailtonSantos <nailton0santos0@gmail.com>
2024-11-23 10:14:25 -0300

    o teste de aleatorização de mantel foi aplicado de uma maneira errada e presica ser refeita - entao apaguei para refazer

--- scripts/analise_renda/Matriz_mantel.R
+++ scripts/analise_renda/Matriz_mantel.R
@@ -5,16 +5,3 @@ shapiro.test(renda_data$PIB_2016_percapta) ## os dados nao seguem uma distribui�
 
 ## =======================================================  calculando matriz_distancia A =======================================
 
-matriz_cidades <- as.matrix(cbind(distribuicao_renda$LONGITUDE, distribuicao_renda$LATITUDE))
-
-A <- spDists(x = matriz_cidades, y = matriz_cidades, longlat = FALSE)
-
-## =========================================== calculando matriz diferenca entre pib B ==========================================
-
-B <- outer(distribuicao_renda$PIB_2016_percapta, distribuicao_renda$PIB_2016_percapta, function(x,y) sqrt((x-y)^2))
-
-## ============================================ rodando Teste de Mantel para detecção de padrão espacial ========================
-
-options(mc.cores = parallel::detectCores())
-
-mantel_teste <- mantel(A, B, permutations = 100000) ## 
